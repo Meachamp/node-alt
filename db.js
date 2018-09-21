4cde971ad3b17e3d18ef769ec1a4341c4bac9669
Meachamp <34523297+Meachamp@users.noreply.github.com>
2018-09-21 04:46:49 -0400

    Fix database module

--- db.js
+++ db.js
@@ -2,5 +2,5 @@ const neo4j = require('neo4j-driver').v1
 const util = require('util')
 const config = require('./config')
 
-module.exports = driver = neo4j.driver(util.format('bolt://%s', config.database.host), 
+module.exports = neo4j.driver(util.format('bolt://%s', config.database.host), 
                 neo4j.auth.basic(config.database.username, config.database.password))
