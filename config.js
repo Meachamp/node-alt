01ce354475d05b13e3bc2aec53d58716be3d7846
Meachamp <34523297+Meachamp@users.noreply.github.com>
2018-09-21 04:45:37 -0400

    Config and database

--- config.js
+++ config.js
@@ -1,7 +1,12 @@
 let config = {}
 config.database = {}
+config.server = {}
+
 config.database.username = 'neo4j'
 config.database.password = 'neo4j'
 config.database.host = 'localhost'
+
+config.server.listenPort = 4000
+
 
 module.exports = config
--- db.js
+++ db.js
@@ -1,7 +1,6 @@
-const neo4j = require('neo4j').v1
+const neo4j = require('neo4j-driver').v1
 const util = require('util')
 const config = require('./config')
 
-let driver = neo4j.driver(util.format('bolt://%s', config.database.host), 
+module.exports = driver = neo4j.driver(util.format('bolt://%s', config.database.host), 
                 neo4j.auth.basic(config.database.username, config.database.password))
-
