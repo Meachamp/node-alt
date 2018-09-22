34078b788ec8c4afd1c75e4edd11896e623fadf2
Meachamp <34523297+Meachamp@users.noreply.github.com>
2018-09-22 00:32:47 -0400

    Update config

--- config.js
+++ config.js
@@ -1,10 +1,17 @@
 let config = {}
 config.database = {}
 config.server = {}
+config.sql = {}
 
 config.database.username = 'neo4j'
 config.database.password = 'test'
 config.database.host = 'localhost'
+
+config.sql.username = 'test'
+config.sql.password = 'test'
+config.sql.database = 'alt'
+config.sql.host = 'localhost'
+config.sql.port = 3306
 
 config.server.listenPort = 4000
 
